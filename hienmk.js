82f5e89fb50d1c5c017c7590889b66023f64f1cd
LightDev81 <vuilachinhmvp@gmail.com>
2024-11-05 02:45:44 +0700

    xong

---
+++ hienmk.js
@@ -0,0 +1,9 @@
+function togglePassword(fieldId, icon) {
+    const passwordField = document.getElementById(fieldId);
+    const isPasswordVisible = passwordField.type === "text";
+
+    passwordField.type = isPasswordVisible ? "password" : "text";
+
+    icon.classList.toggle("fa-eye-slash");
+    icon.classList.toggle("fa-eye");
+}
