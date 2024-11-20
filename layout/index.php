cd45f93224de71dd84c654c9944d39a736b1e5af
MTuans <tuandmps40831@gmail.com>
2024-11-21 02:08:36 +0700

    oke

--- layout/index.php
+++ layout/index.php
@@ -5,6 +5,9 @@ include "view/header.php" ;
 $act=isset($_GET['act']) ? $_GET['act'] : 'index';
 $id=isset($_GET['id']) ? $_GET['id']:'';
 $idtl=isset($_GET['idtl']) ? $_GET['idtl']:'';
+$idtg=isset($_GET['idtg']) ? $_GET['idtg']:'';
+$idnxb=isset($_GET['idnxb']) ? $_GET['idnxb']:'';
+
 $action=isset($_GET['action']) ? $_GET['action']:'';
 $ten=isset($_POST['ten']) ? $_POST['ten']:'';
 $gia=isset($_POST['gia']) ? $_POST['gia']:'';
