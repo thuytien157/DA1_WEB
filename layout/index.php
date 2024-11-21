54fa30420e8f804faace7a816accd4054ecd0716
LightDev81 <vuilachinhmvp@gmail.com>
2024-11-21 19:30:53 +0700

    done

--- layout/index.php
+++ layout/index.php
@@ -68,9 +68,9 @@ switch ($act){
 
     case 'tienhanhdathang':
         include_once 'controller/dathangController.php';
-        $dathangController=new dathangController($action);        
+        $dathangController=new dathangController($action);
         break;
-    
+
 
     case 'lichsu':
         include_once 'controller/donhangController.php';
@@ -107,6 +107,10 @@ switch ($act){
         // Chuyển hướng người dùng về trang đăng nhập
         header("Location: index.php?act=login");
         exit();
+        break;
+
+    case 'readmin':
+        header("Location: ../layoutAdmin/index.php");
         break;
 }
 
