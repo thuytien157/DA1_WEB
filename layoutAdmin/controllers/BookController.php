5324afbdcb4f8abd9d8059f3c7dcb4be965aa047
thuytien <tiennttps39163@gmail.com>
2024-12-03 14:08:31 +0700

    oke rồi

--- layoutAdmin/controllers/BookController.php
+++ layoutAdmin/controllers/BookController.php
@@ -25,11 +25,15 @@ class BookController {
         require_once "Views/book.php";
     }
 
-    public function addBooks($id_theloai, $id_tacgia, $id_nxb, $ten_sach, $hinh, $gia, $giam, $mo_ta, $nam_xb, $so_luong_ban) {
-        $result = $this->bookModel->addBooks($id_theloai, $id_tacgia, $id_nxb, $ten_sach, $hinh, $gia, $giam, $mo_ta, $nam_xb, $so_luong_ban);
-        return $result;
-        require_once "Views/book.php";
+    public function addBooks($id_theloai, $id_tacgia, $id_nxb, $ten_sach, $hinh, $gia, $giam, $mo_ta, $nam_xb, $so_luong_ban, $nha_cung_cap, $ngon_ngu, $trong_luong, $kich_thuoc, $so_luong_trang, $hinh_thuc, $so_luong) {
+        $id_sach = $this->bookModel->addBooks($id_theloai, $id_tacgia, $id_nxb, $ten_sach, $hinh, $gia, $giam, $mo_ta, $nam_xb, $so_luong_ban);
+        if($id_sach){
+            $this->bookModel->add_DetailBooks($id_sach, $nha_cung_cap, $ngon_ngu, $trong_luong, $kich_thuoc, $so_luong_trang, $hinh_thuc, $so_luong);
+        }
+        header('location: index.php?page=book');
+        exit();    
     }
+    
     public function updateBook($id, $id_theloai, $id_tacgia, $id_nxb, $ten_sach, $hinh, $gia, $giam, $mo_ta, $nam_xb, $so_luong_ban) {
         $result = $this->bookModel->updateBook($id, $id_theloai, $id_tacgia, $id_nxb, $ten_sach, $hinh, $gia, $giam, $mo_ta, $nam_xb, $so_luong_ban);
         
