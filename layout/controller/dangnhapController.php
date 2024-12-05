e0067d003e67b4f924c30a9a43968f6029fead1f
thuytien <tiennttps39163@gmail.com>
2024-12-05 22:15:48 +0700

    thêm mã hoá password và file database

--- layout/controller/dangnhapController.php
+++ layout/controller/dangnhapController.php
@@ -12,7 +12,7 @@ class dangnhapController {
         }
 
         // Xử lý đăng nhập khi POST request
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['dn'])) {
             $user = $_POST['user'] ?? '';  // Lấy tên đăng nhập từ POST
             $password = $_POST['password'] ?? '';  // Lấy mật khẩu từ POST
             $this->handleLogin($user, $password);
@@ -22,8 +22,12 @@ class dangnhapController {
     private function handleLogin($user, $password) {
         // Lấy thông tin người dùng dựa trên tên đăng nhập
         $userData = $this->dangnhap->login($user);
-        // var_dump(password_verify($password,$userData['mat_khau']));
-        if ($userData && $password==$userData['mat_khau']) {
+
+         var_dump(password_verify($password,$userData['mat_khau']));
+         var_dump(password_hash("Thuytien965002@", PASSWORD_BCRYPT));
+         var_dump($userData['mat_khau']);
+        $password = $_POST['password'];
+        if ($userData && password_verify($password, $userData['mat_khau'])) {
             // Đăng nhập thành công, lưu thông tin vào session
             session_start();
             $_SESSION['user'] = [
@@ -38,7 +42,7 @@ class dangnhapController {
             $_SESSION['id'] = $userData['id'];
 
             // Chuyển hướng theo vai trò người dùng
-            if ($userData['vai_tro'] == 1| $userData['vai_tro'] == 2)  {
+            if ($userData['vai_tro'] == 1 || $userData['vai_tro'] == 2)  {
                 header("Location: ../layoutAdmin/index.php");
             } else {
                 header("Location: index.php");
